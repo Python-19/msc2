b4d89b1353a52622fb9004ec9be5a3a03d051eab
Python-19 <90200599+Python-19@users.noreply.github.com>
2022-04-06 23:55:35 -0700

    Add files via upload

---
+++ SNA/Practical 2/practical 2.R
@@ -0,0 +1,11 @@
+library(igraph)
+getwd()
+nodes <- read.csv("nodes.csv")
+head(nodes)
+links <- read.csv("edges.csv")
+head(links)
+net <- graph.data.frame(d=links, vertices=nodes, directed=T)
+m=as.matrix(net)
+get.adjacency(m)
+plot(net)
+
